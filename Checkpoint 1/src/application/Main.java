c835f4180858e70144d77f0af219ec2ac513c977
jreiling19 <jreiling19@OlinLab204-06.augustana.edu>
2021-10-07 09:55:12 -0500

    Merged Grant's text box with Main.java file in Checkpoint1

--- Checkpoint 1/src/application/Main.java
+++ Checkpoint 1/src/application/Main.java
@@ -2,9 +2,11 @@ package application;
 	
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 
@@ -15,7 +17,8 @@ public class Main extends Application {
 		try {
 			//make gridPaneMain
 			GridPane gridPaneMain = new GridPane();
-			Scene scene = new Scene(gridPaneMain, 600, 300);
+			gridPaneMain.setAlignment(Pos.CENTER);
+			Scene scene = new Scene(gridPaneMain, 900, 300);
 			primaryStage.setTitle("My Schedule");
 			gridPaneMain.setPadding(new Insets(10));
 			gridPaneMain.setHgap(15);
@@ -23,10 +26,17 @@ public class Main extends Application {
 			
 			//title for window
 			Label wordLabel = new Label("Fall Semester Schedule 2021");
-			wordLabel.setFont(new Font("Arial", 30));
+			wordLabel.setFont(new Font("Arial", 20));
 			gridPaneMain.add(wordLabel, 0, 0);
 			
+			//name of text box
+			Label schedule = new Label("Insert Class Schedule:");
+			gridPaneMain.add(schedule, 0, 2);
 
+			//Creates text box
+			TextArea userTextField = new TextArea();
+			gridPaneMain.add(userTextField, 1, 2);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
