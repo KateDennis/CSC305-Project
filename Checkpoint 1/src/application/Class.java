d6e46bce71fc9f3380ac96446926fc5f1960c7df
katelyndennis20 <katelyndennis20@OlinLab204-05.augustana.edu>
2021-10-19 09:56:47 -0500

    Adding on extra parts because boredom an class time, but we might use them later

--- Checkpoint 1/src/application/Class.java
+++ Checkpoint 1/src/application/Class.java
@@ -1,19 +1,25 @@
 package application;
 
 public class Class {
+	private String classCode = new String();
 	private String className = new String();
+	private String startDate = new String();
+	private String endDate = new String();
 	private String startTime = new String();
 	private String endTime = new String();
 	private String days = new String();
 
-	public Class(String name, String startTime, String endTime, String days) {
+	public Class(String code, String name, String startDate, String endDate, String startTime, String endTime, String days) {
+		classCode = code;
 		className = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
 		this.startTime = startTime;
 		this.endTime = endTime;
 		this.days = days;
 	}
 	
 	public String toString() {
-		return className + ", " + startTime + ", " + endTime + ", " + days;
+		return classCode + ", " + className + ", " + startDate + ", " + endDate + ", " + startTime + ", " + endTime + ", " + days;
 	}
 }
--- Checkpoint 1/src/application/SubmitHandler.java
+++ Checkpoint 1/src/application/SubmitHandler.java
@@ -5,20 +5,30 @@ import java.util.*;
 public class SubmitHandler {
 	
 	/**
+	 * Creates a Submit Handler which parses the data inputed into main text box
 	 * 
-	 * @param schedule
+	 * @param schedule is the list of the input from main
 	 */
 	public SubmitHandler(String[] schedule) {
 		int i = 1;
+		String code = "";
 		String name = "";
+		String startDate = "";
+		String endDate = "";
 		ArrayList<Class> classList = new ArrayList<Class>();
 		ArrayList<String> buildingList = createBuildingList();
 		for (String line : schedule) {
 			if (i == 1) {
-				name = line;
+				code = line;
+				name = schedule[i];
 			}
 			if (buildingList.contains(line) && schedule.length > i + 2 && schedule[i + 2] != null) {
-				name = schedule[i + 2];
+				code = schedule[i + 2];
+				name = schedule[i + 3];
+			}
+			if (line.startsWith("0")) {
+				startDate = line;
+				endDate = schedule[i];
 			}
 			if (line.contains("M ") && !(line.contains("-")) || line.contains("Tu ") || line.contains("W ") || line.contains("Th ")
 					|| line.contains("F ")) {
@@ -26,7 +36,7 @@ public class SubmitHandler {
 				String[] times = schedule[i].split(" ");
 				String startTime = times[0];
 				String endTime = times[2];
-				classList.add(new Class(name, startTime, endTime, days));
+				classList.add(new Class(code, name, startDate, endDate, startTime, endTime, days));
 			}
 			i++;
 		}
@@ -37,11 +47,12 @@ public class SubmitHandler {
 	
 	/**
 	 * Creates an ArrayList<String> of the campus buildings
+	 * 
 	 * @return ArrayList<String> of Augustana College Buildings
 	 */ 
 	public ArrayList<String> createBuildingList() {
 		ArrayList<String> list = new ArrayList<String>();
-		list.add("EVLD"); 
+		list.add("EVLD");
 		list.add("SORN");
 		list.add("OLIN");
 		list.add("LIND");
