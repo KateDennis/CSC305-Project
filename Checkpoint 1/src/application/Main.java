7637923d26fd291a840e8e48564ba0e65c5c9d8e
jreiling19 <jreiling19@OlinLab204-06.augustana.edu>
2021-11-04 09:26:13 -0500

    adf sdf

--- Checkpoint 1/src/application/Main.java
+++ Checkpoint 1/src/application/Main.java
@@ -21,8 +21,8 @@ public class Main extends Application {
 			Scene scene = new Scene(gridPaneMain, 900, 300);
 			primaryStage.setTitle("My Schedule");
 			gridPaneMain.setPadding(new Insets(10));
-			gridPaneMain.setHgap(15); 
-			gridPaneMain.setVgap(15); 
+			gridPaneMain.setHgap(15);
+			gridPaneMain.setVgap(15);
 			
 			//title for window
 			Label wordLabel = new Label("Arches Semester Data to Calender");
