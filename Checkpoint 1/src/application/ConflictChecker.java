046c97475fa6acfc7b8529c48da21f1255cde86a
KateDennis <90398630+KateDennis@users.noreply.github.com>
2021-12-01 16:04:20 -0600

    Update ConflictChecker.java
    Adding comments to loops and else ifs

--- Checkpoint 1/src/application/ConflictChecker.java
+++ Checkpoint 1/src/application/ConflictChecker.java
@@ -13,11 +13,19 @@ public class ConflictChecker {
 	 * @param courseList - the array of courses a student is taking
 	 */
 	public ConflictChecker(ArrayList<Course> courseList) {
+		
+		//getting the intial course to check the remaining courses against
 		for (int courseIndex = 0; courseIndex < courseList.size(); courseIndex++) {
 			Course course = courseList.get(courseIndex);
+			
+			//going through each course to check against the intial course for any conflicts
 			for (int i = courseIndex + 1; i < courseList.size(); i++) {
+				
+				//removing the checked course if identical to original course
 				if (course.equals(courseList.get(i))) {
 					courseList.remove(i);
+					
+				//alerting the user that two or more courses conflict with each other
 				} else if (course.compareTo(courseList.get(i)) > 0) {
 					conflictList.add("Course " + course.getName() + " and Course " + courseList.get(i).getName()
 							+ " have conflicting times");
